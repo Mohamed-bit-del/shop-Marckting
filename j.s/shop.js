52e8cd064399475c1780e48da4ea20e5eb4ddbe2
Mohamed <tmido2890@gmail.com>
2021-03-31 21:07:28 +0200

    puch

--- j.s/shop.js
+++ j.s/shop.js
@@ -5,3 +5,19 @@ function openBtn() {
 function closeBtn() {
   document.getElementById('open-nav').style.left = '-310px';
 }
+
+ function verticleList() {
+  document.getElementById('verticle-list').className = "list-view";
+ }
+
+function horizontalList() {
+  document.getElementById('verticle-list').className = 'proudact-shop__cards'
+}
+
+function filter() {
+  document.getElementById('filter').style.left = '0px'
+}
+
+function leftList() {
+  document.getElementById('filter').style.left = '-350px'
+}
